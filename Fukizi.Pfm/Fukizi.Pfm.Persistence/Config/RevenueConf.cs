53baed1eb708de12df9b6536b7d754e4132ad86d
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-21 13:47:30 +0200

    Configure revenue mapping

--- Fukizi.Pfm/Fukizi.Pfm.Persistence/Config/RevenueConf.cs
+++ Fukizi.Pfm/Fukizi.Pfm.Persistence/Config/RevenueConf.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Fukizi.Pfm.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Fukizi.Pfm.Persistence.Config
 {
-    class RevenueConf
+    public class RevenueConf : IEntityTypeConfiguration<Revenue>
     {
+       public void Configure(EntityTypeBuilder<Revenue> builder)
+       {
+          builder.HasKey(r => r.Id);
+          builder.Property(e => e.Amount).HasColumnName("Amount");
+          builder.Property(r => r.CategoryId).HasColumnName("CategoryId");
+          builder.Property(r => r.PayMethodId).HasColumnName("PayMethodId");
+          builder.Property(r => r.Date).HasColumnName("Date");
+         //ToDo: foreign key mapping
+          builder.ToTable("Revenue", "Accounting");
+       }
     }
 }
