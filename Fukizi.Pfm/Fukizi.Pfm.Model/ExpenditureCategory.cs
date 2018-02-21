2a56b72a1f6c1554b7bfe46f78de40d036301357
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-21 13:38:01 +0200

    ExpenditureCategory Inherit from Category and ExpenditureCategory mapping

--- Fukizi.Pfm/Fukizi.Pfm.Model/ExpenditureCategory.cs
+++ Fukizi.Pfm/Fukizi.Pfm.Model/ExpenditureCategory.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Fukizi.Pfm.Model
 {
-    public class ExpenditureCategory
+    public class ExpenditureCategory : Category, IComparable<ExpenditureCategory>
     {
+       public ExpenditureCategory(int id, string categoryName) : base(id, categoryName)
+       {
+       }
+
+       public int CompareTo(ExpenditureCategory other)
+       {
+          return string.CompareOrdinal(Name, other.Name);
+       }
+
+       public override string ToString()
+       {
+          return Name;
+       }
+
+       public bool Equals(ExpenditureCategory  expenditurecateGory)
+       {
+          return Id == expenditurecateGory.Id && Name == expenditurecateGory.Name;
+       }
     }
 }
--- Fukizi.Pfm/Fukizi.Pfm.Persistence/Config/ExpenditureCategoryConf.cs
+++ Fukizi.Pfm/Fukizi.Pfm.Persistence/Config/ExpenditureCategoryConf.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Fukizi.Pfm.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Fukizi.Pfm.Persistence.Config
 {
-    class ExpenditureCategoryConf
+    public class ExpenditureCategoryConf : IEntityTypeConfiguration<ExpenditureCategory>
     {
+       public void Configure(EntityTypeBuilder<ExpenditureCategory> builder)
+       {
+          builder.HasKey(e => e.Id);
+          builder.Property(e => e.Id).HasColumnName("Id");
+          builder.Property(e => e.Name).HasColumnName("Name");
+          builder.ToTable("ExpenditureCategory", "Accounting");
+       }
     }
 }
