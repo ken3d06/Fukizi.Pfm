f4ffe01cf59b683520d4c7dfd1afcf3ed0d10ced
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-22 17:51:36 +0200

    Both expenditure and revenue calculation services

--- Fukizi.Pfm/Fukizi.Pfm.Services/MonthRevenueExpenditureService.cs
+++ Fukizi.Pfm/Fukizi.Pfm.Services/MonthRevenueExpenditureService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Fukizi.Pfm.Common.Validations;
 using Fukizi.Pfm.Model;
 using Fukizi.Pfm.Persistence;
 using Fukizi.Pfm.Repository;
@@ -49,6 +50,60 @@ namespace Fukizi.Pfm.Services
       {
          return _revenueService.GetTotalForCategoryForGivenMonth(month, categoryName);
       }
+      #endregion
+
+      #region BothRevenueAndExpenditure
+
+      public decimal GetTotalForCategoryAndMonth(string categoryType, string categoryName, DateTime month)
+      {
+         ValidationContract.Required<ArgumentException>(!string.IsNullOrWhiteSpace(categoryType), "Category type must be specified");
+         ValidationContract.Required<ArgumentException>(!string.IsNullOrWhiteSpace(categoryName), "Category name must be specified");
+
+            switch (categoryType?.ToLower())
+            {
+               case "expenditure":
+               {
+                  return GetExpenditureTotalForCategoryAndMonth(categoryName, month);
+               }
+               case "revenue":
+               {
+                  return GetRevenueTotalForCategoryAndMonth(categoryName, month);
+               }
+               default:
+               {
+                  return 0;
+               }
+            }
+      }
+
+      public Dictionary<string, decimal> GetTotalFlowPerCategoriesForMonth(DateTime month)
+      {
+         var categoryTotals = new Dictionary<string, decimal>
+         {
+            {"Total Expenditures", GetTotalExpenditureForMonth(month)},
+            {"Total Revenue", GetTotalRevenueForMonth(month)}
+         };
+         //ToDo: add this range to dictionary
+         //categoryTotals.AddRange(_expenditureService.GetExpenditureTotalsForAllCategoriesForMonth(month));
+
+         foreach (var totalRevenueByCategory in _revenueService.GetRevenueTotalsForAllCategoriesForMonth(month))
+         {
+            if (categoryTotals.ContainsKey(totalRevenueByCategory.Key))
+            {
+               var placeholder = categoryTotals[totalRevenueByCategory.Key];
+               categoryTotals.Remove(totalRevenueByCategory.Key);
+               categoryTotals.Add($"{totalRevenueByCategory.Key} - Expenditure", placeholder);
+               categoryTotals.Add($"{totalRevenueByCategory.Key} - revenue", totalRevenueByCategory.Value);
+            }
+            else
+            {
+               categoryTotals.Add(totalRevenueByCategory.Key, totalRevenueByCategory.Value);
+            }
+         }
+
+         return categoryTotals;
+      }
+
       #endregion
 
    }
