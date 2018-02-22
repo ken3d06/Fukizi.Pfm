80c58d6324a04c95ab87dffbeb49f473f050d8d7
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-22 11:14:00 +0200

    revenue service stub

---
+++ Fukizi.Pfm/Fukizi.Pfm.Services/RevenueService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fukizi.Pfm.Model;
+using Fukizi.Pfm.Repository;
+using Fukizi.Pfm.Services.Interfaces;
+
+namespace Fukizi.Pfm.Services
+{
+    public class RevenueService : ITransactionsService
+    {
+       private readonly RevenueRepository _revenueRepository;
+
+       public RevenueService(RevenueRepository repository)
+       {
+          _revenueRepository = repository;
+       }
+       public void Create(Transaction transaction)
+       {
+          throw new NotImplementedException();
+       }
+
+       public void Save(Transaction transaction)
+       {
+          throw new NotImplementedException();
+       }
+
+       public IEnumerable<Transaction> GetTransactions()
+       {
+          throw new NotImplementedException();
+       }
+    }
+}
