bb6cb7b80e5f57b5d586cd3c5ee7a6b0c98790eb
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-22 10:56:12 +0200

    Transaction extension TryParseToExpenditure

---
+++ Fukizi.Pfm/Fukizi.Pfm.Services/TransactionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fukizi.Pfm.Model;
+
+namespace Fukizi.Pfm.Services
+{
+    public class TransactionExtensions
+    {
+       public static Expenditure TryParseToExpenditure(Transaction t)
+       {
+          if (!(t is Expenditure exp))
+          {
+             throw new ArgumentException("This is a wrong type of transaction");
+          }
+          exp.Id = t.Id;
+          exp.Amount = t.Amount;
+          exp.Date = t.Date;
+          exp.Description = t.Description;
+          exp.PayMethod = t.PayMethod;
+          exp.PayMethodId = t.PayMethodId;
+          exp.CategoryId = t.CategoryId;
+          exp.Category = t.Category != null ? new ExpenditureCategory(t.Category.Id, t.Category.Name) : null;
+          return exp;
+       }
+   }
+}
