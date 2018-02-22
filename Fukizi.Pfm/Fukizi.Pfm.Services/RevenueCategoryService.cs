1671e8557a6fc96c402dd5ce5c612a77e523f34d
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-22 16:00:51 +0200

    Revenue category service stub

---
+++ Fukizi.Pfm/Fukizi.Pfm.Services/RevenueCategoryService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fukizi.Pfm.Model;
+
+namespace Fukizi.Pfm.Services
+{
+    public class RevenueCategoryService : ICategoryService<RevenueCategory>
+    {
+       public IEnumerable<Category> GetAllCategories()
+       {
+          throw new NotImplementedException();
+       }
+
+       public bool Exists(string name)
+       {
+          throw new NotImplementedException();
+       }
+
+       public RevenueCategory Create(string name, int id = 0)
+       {
+          throw new NotImplementedException();
+       }
+
+       public void Delete(string name)
+       {
+          throw new NotImplementedException();
+       }
+
+       public void Save(int id, string name)
+       {
+          throw new NotImplementedException();
+       }
+    }
+}
