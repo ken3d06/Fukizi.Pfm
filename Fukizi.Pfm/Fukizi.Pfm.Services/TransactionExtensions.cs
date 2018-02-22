5faa821b12f712284651414cdb2fc1c7bc88517d
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-22 11:05:56 +0200

    TryParseToRevenue extension method

--- Fukizi.Pfm/Fukizi.Pfm.Services/TransactionExtensions.cs
+++ Fukizi.Pfm/Fukizi.Pfm.Services/TransactionExtensions.cs
@@ -7,21 +7,37 @@ namespace Fukizi.Pfm.Services
 {
     public class TransactionExtensions
     {
-       public static Expenditure TryParseToExpenditure(Transaction t)
+       public static Expenditure TryParseToExpenditure(Transaction transaction)
        {
-          if (!(t is Expenditure exp))
+          if (!(transaction is Expenditure expenditure))
           {
              throw new ArgumentException("This is a wrong type of transaction");
           }
-          exp.Id = t.Id;
-          exp.Amount = t.Amount;
-          exp.Date = t.Date;
-          exp.Description = t.Description;
-          exp.PayMethod = t.PayMethod;
-          exp.PayMethodId = t.PayMethodId;
-          exp.CategoryId = t.CategoryId;
-          exp.Category = t.Category != null ? new ExpenditureCategory(t.Category.Id, t.Category.Name) : null;
-          return exp;
+          expenditure.Id = transaction.Id;
+          expenditure.Amount = transaction.Amount;
+          expenditure.Date = transaction.Date;
+          expenditure.Description = transaction.Description;
+          expenditure.PayMethod = transaction.PayMethod;
+          expenditure.PayMethodId = transaction.PayMethodId;
+          expenditure.CategoryId = transaction.CategoryId;
+          expenditure.Category = transaction.Category != null ? new ExpenditureCategory(transaction.Category.Id, transaction.Category.Name) : null;
+          return expenditure;
+       }
+       public static Revenue TryParseToRevenue(Transaction transaction)
+       {
+          if (!(transaction is Revenue revenue))
+          {
+             throw new ArgumentException("The transaction is the wrong type");
+          }
+          revenue.Id = transaction.Id;
+          revenue.Amount = transaction.Amount;
+          revenue.Date = transaction.Date;
+          revenue.Description = transaction.Description;
+          revenue.PayMethod = transaction.PayMethod;
+          revenue.PayMethodId = transaction.PayMethodId;
+          revenue.CategoryId = transaction.CategoryId;
+          revenue.Category = transaction.Category != null ? new RevenueCategory(transaction.Category.Id, transaction.Category.Name) : null;
+          return revenue;
        }
    }
 }
