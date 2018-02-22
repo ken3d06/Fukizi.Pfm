1e884abe893de243f7e694be87c4c5bb175aff11
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-22 10:48:32 +0200

    Interface for transactions service and namespace change for ICategoryService

---
+++ Fukizi.Pfm/Fukizi.Pfm.Services/Interfaces/ITransactionsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fukizi.Pfm.Model;
+
+namespace Fukizi.Pfm.Services.Interfaces
+{
+    public interface ITransactionsService
+    {
+       void Create(Transaction transaction);
+       void Save(Transaction transaction);
+       IEnumerable<Transaction> GetTransactions();
+   }
+}
