6350233180f71b4ca0ebef0a993fdf6e93a1f633
Kenneth Fukizi <kennethfukizi@ymail.com>
2018-02-22 11:33:53 +0200

    Requires validation contract

---
+++ Fukizi.Pfm/Fukizi.Pfm.Common/Validations/ValidationContract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace Fukizi.Pfm.Common.Validations
+{
+    public class ValidationContract
+    {
+       public static void Requires<TException>(bool predicate, string message = "") where TException : Exception, new()
+       {
+          if (predicate) return;
+          Debug.WriteLine(message);
+          var ex = new TException();
+          ex.GetType().GetField("_message", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(ex, message);
+          throw ex;
+       }
+   }
+}
